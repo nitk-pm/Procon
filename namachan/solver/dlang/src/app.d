c683db70f217b943cff2dcce0ea316564d139d58
Nakano Masaki <namachan10777@gmail.com>
2017-07-17 01:02:00 +0900

    enable to react argments.

--- namachan/solver/dlang/src/app.d
+++ namachan/solver/dlang/src/app.d
@@ -1,5 +1,16 @@
-import std.stdio;
+module procon28.app;
 
-void main(){
-	writeln("Edit source/app.d to start your project.");
+import std.getopt : getopt;
+import std.algorithm.iteration : splitter;
+import std.file : readText;
+
+import procon28.decoder;
+
+void main(string[] args){
+	string piece_file_name = "";
+	string frame_file_name = "";
+	args.getopt ("p", &piece_file_name, "f", &frame_file_name);
+	if (piece_file_name == "" || frame_file_name == "") throw new Exception ("solver need piece and frame defination file");
+	auto frame = frame_file_name.readText.decode_frame;
+	auto piece = piece_file_name.readText.decode_piece;
 }
