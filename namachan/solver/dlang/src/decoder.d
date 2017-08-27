90a3c0eadc2430851ac1d0ecdf44f1c58694d61d
Nakano Masaki <namachan10777@gmail.com>
2017-08-27 11:33:31 +0900

    fix decoder
    json schemaと全く噛み合ってなかったので修正

--- namachan/solver/dlang/src/decoder.d
+++ namachan/solver/dlang/src/decoder.d
@@ -6,37 +6,38 @@ import procon28.basic_data : Segment, Piece, Shape;
 import procon28.solver.data : vertexies2shape;
 
 import std.conv : to;
-import std.json : parseJSON;
+import std.json : parseJSON, JSONValue;
+
+Shape decode_shape (in JSONValue json) {
+	Vector2i[] vertexies;
+	foreach (pos; json.array) {
+		auto x = pos.object["x"].integer.to!int;
+		auto y = pos.object["y"].integer.to!int;
+		vertexies ~= Vector2i(x, y);
+	}
+	return vertexies2shape(vertexies);
+}
 
 ///jsonフォーマットの文字列をShapeにデコード
 Shape decode_frame (in string str) {
 	auto json = str.parseJSON;
-	Vector2i[] vertexies;
-	foreach (pair; json["frame"].object["shape"].array) {
-		auto x = pair.array[0].integer.to!int;
-		auto y = pair.array[1].integer.to!int;
-		vertexies ~= Vector2i(x, y);
+	Shape shape;
+	foreach (shape_json; json["shapes"].array) {
+		shape ~= decode_shape (shape_json);
 	}
-	return vertexies2shape (vertexies);
+	return shape;
 }
 
 ///jsonフォーマットの文字列をPieceの集合にデコード
 Piece[] decode_piece (in string str) {
 	auto json = str.parseJSON;
-
 	Piece[] piecies;
-	foreach (piece_json; json["piece"].array) {
-		foreach (shape_json; piece_json.array) {
-			Piece piece;
-			Vector2i[] vertexies;
-			foreach (pair; shape_json.array) {
-				auto x = pair.array[0].integer.to!int;
-				auto y = pair.array[1].integer.to!int;
-				vertexies ~= Vector2i(x, y);
-			}
-			auto shape = vertexies2shape (vertexies);
-			piece ~= shape;
+	foreach (piece_json; json["pieces"].array) {
+		Shape[] shapes;
+		foreach (shape_json; piece_json["shapes"].array) {
+			shapes ~= decode_shape (shape_json);
 		}
+		piecies ~= shapes;
 	}
 	return piecies;
 }
