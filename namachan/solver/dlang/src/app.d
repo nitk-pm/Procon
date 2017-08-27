69612d6038366a0da93e8a6cdf0f2a1426782536
Nakano Masaki <namachan10777@gmail.com>
2017-08-27 11:21:04 +0900

    add -w option

--- namachan/solver/dlang/src/app.d
+++ namachan/solver/dlang/src/app.d
@@ -5,11 +5,14 @@ import std.algorithm.iteration : splitter;
 import std.file : readText;
 
 import procon28.decoder;
+import procon28.solver.search;
+import procon28.solver.eval;
 
 void main(string[] args){
 	string piece_file_name = "";
 	string frame_file_name = "";
-	args.getopt ("p", &piece_file_name, "f", &frame_file_name);
+	int width = 100;
+	args.getopt ("p", &piece_file_name, "f", &frame_file_name, "w", &width);
 	if (piece_file_name == "" || frame_file_name == "") throw new Exception ("solver need piece and frame defination file");
 	auto frame = frame_file_name.readText.decode_frame;
 	auto piece = piece_file_name.readText.decode_piece;
