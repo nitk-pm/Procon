a16c6791aeb1a51992c3b5a7c83f231b8f92b65b
rucm <st16423@kagawa.kosen-ac.jp>
2017-09-06 22:40:49 +0900

    ファイル監視サンプルコード

---
+++ tools/watcher/test.py
@@ -0,0 +1,35 @@
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+
+class ChangeHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+        if event.is_directory:
+            return
+        print('created: {}'.format(event.src_path))
+
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+        print('modified: {}'.format(event.src_path))
+
+    def on_deleted(self, event):
+        if event.is_directory:
+            return
+        print('deleted: {}'.format(event.src_path))
+
+
+if __name__ in '__main__':
+    while True:
+        event_handler = ChangeHandler()
+        observer = Observer()
+        observer.schedule(event_handler, './', recursive=True)
+        observer.start()
+        try:
+            import time
+            while True:
+                time.sleep(1)
+        except:
+            observer.stop()
+        observer.join()
