e0ceaeb2ab17cca0b3a884f62c90abd8b46d5b79
Nakano Masaki <namachan10777@gmail.com>
2017-09-07 15:21:58 +0900

    add vertex based eval function
    頂点ベースの評価関数の追加

--- solver/namachan/src/solver/eval.d
+++ solver/namachan/src/solver/eval.d
@@ -1,7 +1,7 @@
 module procon28.solver.eval;
 
-import procon28.basic_data : P, S, Shape;
-import procon28.solver.datamanip : equal_slope, move, vertexies2shape, protrude_frame;
+import procon28.basic_data : P, S, Shape, Segment;
+import procon28.solver.datamanip : equal_slope, move, vertexies2shape, protrude_frame, judge_on_line;
 
 /++
  + 線分の角度の一致度と重複した点の数で評価する評価関数
@@ -35,4 +35,18 @@ unittest {
 	auto frame = [P (10, 10), P (110, 10), P (60, 110)].vertexies2shape;
 	auto shape = [P (0, 0), P (20, 0), P(10, 20)].vertexies2shape;
 	assert (angle_and_points (frame, shape.move(P (10, 10))) == 3);
+}
+
+float eval_basic (in P[] frame, in P[] piece) {
+	float point_conflict = 0.0f;
+	foreach (piece_point; piece) {
+		auto frame_point2 = frame[$-1];
+		foreach (frame_point1; frame) {
+			if (frame_point1 == piece_point)
+				++point_conflict;
+			else if (judge_on_line(piece_point, Segment(frame_point1, frame_point2)))
+				++point_conflict;
+		}
+	}
+	return point_conflict;
 }
