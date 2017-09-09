83f683bae7ae22a91eb966d69ff5cf3ec23317e0
rucm <st16423@kagawa.kosen-ac.jp>
2017-09-09 23:10:13 +0900

    ディレクトリ監視機能をViewerに統合する準備開始 windows上でopamが動かないのでpythonでやってみる

---
+++ tools/viewer/test.py
@@ -0,0 +1,5 @@
+import util
+
+if __name__ == '__main__':
+    converter = util.Converter()
+    converter.expand_patterns('3 0 0 10 0 0 10')
--- tools/viewer/util.py
+++ tools/viewer/util.py
@@ -3,10 +3,12 @@ from PyQt5.QtCore import (
     QPointF
 )
 from PyQt5.QtGui import (
-    QPolygonF
+    QPolygonF,
+    QTransform
 )
 
 
+# 空白区切りの整数列から多角形オブジェクトを生成するコンバータ
 class Converter(object):
 
     def __init__(self, data=None):
@@ -40,3 +42,34 @@ class Converter(object):
 
         points.append(points[0])
         return QPolygonF(points)
+
+    def expand_patterns(self, data):
+        from PyQt5.QtGui import QTransform
+        polygon = self.to_polygon(data)
+        pattern_list = []
+        for angle in [0, 90, 180, 270]:
+            pattern_list.append(self.rotate_polygon(polygon, angle))
+            pattern_list.append(self.inverse_polygon(pattern_list[-1]))
+        for i, pattern in enumerate(pattern_list):
+            print('pattern {}'.format(i + 1))
+            for p in pattern:
+                print('({}, {})'.format(p.x(), p.y()))
+
+    def rotate_polygon(self, polygon, angle):
+        c = polygon.boundingRect().center()
+        trans = QTransform()
+        trans.translate(c.x(), c.y())
+        trans.rotate(angle)
+        trans.translate(-c.x(), -c.y())
+        return trans.map(polygon)
+
+    def inverse_polygon(self, polygon):
+        width = polygon.boundingRect().width()
+        trans = QTransform()
+        trans.setMatrix(
+            -1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0
+        )
+        trans.translate(-width, 0)
+        return trans.map(polygon)
