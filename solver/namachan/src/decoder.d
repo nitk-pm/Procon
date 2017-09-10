2912cb7ed488a62d1a0b8018f15ac892e5466de7
Nakano Masaki <namachan10777@gmail.com>
2017-09-11 02:34:11 +0900

    decoder decode as vertex based shape data

--- solver/namachan/src/decoder.d
+++ solver/namachan/src/decoder.d
@@ -1,27 +1,27 @@
 module procon28.decoder;
 
-import procon28.basic_data : Segment, Piece, Shape, Pos;
+import procon28.basic_data : Segment, Piece, Shape, Pos, P;
 import procon28.solver.datamanip : vertexies2shape;
 
 import std.conv : to;
 import std.json : parseJSON, JSONValue;
 
 @system
-pure Shape decode_shape (in JSONValue json) {
+pure P[] decode_shape (in JSONValue json) {
 	Pos[] vertexies;
 	foreach (pos; json.array) {
 		auto x = pos.object["x"].integer.to!int;
 		auto y = pos.object["y"].integer.to!int;
 		vertexies ~= Pos(x, y);
 	}
-	return vertexies2shape(vertexies);
+	return vertexies;
 }
 
 ///jsonフォーマットの文字列をShapeにデコード
 @system
-pure Shape decode_frame (in string str) {
+pure P[][] decode_frame (in string str) {
 	auto json = str.parseJSON;
-	Shape shape;
+	P[][] shape;
 	foreach (shape_json; json["shapes"].array) {
 		shape ~= decode_shape (shape_json);
 	}
@@ -30,11 +30,11 @@ pure Shape decode_frame (in string str) {
 
 ///jsonフォーマットの文字列をPieceの集合にデコード
 @system
-pure Piece[] decode_piece (in string str) {
+pure P[][][] decode_piece (in string str) {
 	auto json = str.parseJSON;
-	Piece[] piecies;
+	P[][][] piecies;
 	foreach (piece_json; json["pieces"].array) {
-		Shape[] shapes;
+		P[][] shapes;
 		foreach (shape_json; piece_json["shapes"].array) {
 			shapes ~= decode_shape (shape_json);
 		}
