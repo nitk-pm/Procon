bff983eed03ccd2d21373f5a436e2a1e9b772795
Unknown <namachan10777@gmail.com>
2017-09-23 16:50:30 +0900

    solver generate output.json
    output.jsonを吐くように。使用はformat/output-schema.yml

---
+++ solver/namachan/src/encoder.d
@@ -0,0 +1,39 @@
+module procon28.encoder;
+
+import std.json : JSONValue;
+import procon28.basic_data : Situation, P, PlacedShape;
+
+@safe
+pure JSONValue pos2json (in P p) {
+	JSONValue json;
+	json["x"] = p.x;
+	json["y"] = p.y;
+	return json;
+}
+
+@safe
+pure JSONValue placed_shape2json (in PlacedShape shape) {
+	JSONValue json;
+	json["piece_id"] = shape.piece_idx;
+	json["shape_id"] = shape.spin_level;
+	json["pos"] = pos2json(P(shape.x, shape.y));
+	return json;
+}
+
+@safe
+pure JSONValue construct_json (in Situation situation) {
+	import std.algorithm.iteration : map;
+	import std.range : array;
+	JSONValue json;
+	json["operations"] =
+		situation
+		.shapes
+		.map!(shape => placed_shape2json(shape))
+		.array;
+	return json;
+}
+
+@safe
+string situation_pp (in Situation situation) {
+	return situation.construct_json.toPrettyString;
+}
