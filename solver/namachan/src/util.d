4d8349da8748c3472e2cbc678797964dc1f6e9b7
Unknown <namachan10777@gmail.com>
2017-10-04 18:39:16 +0900

    add test
    ユニットテスト追加太郎

--- solver/namachan/src/util.d
+++ solver/namachan/src/util.d
@@ -18,4 +18,6 @@ auto qsort (alias comp, T) (in T[] ary) {
 }
 unittest {
 	assert ([5,2,1,4,3].qsort!((a,b) => a < b) == [1,2,3,4,5]);
+	assert ([5,2,1,4,3, 2, 2].qsort!((a,b) => a < b) == [1,2,2,2,3,4,5]);
+	assert ([0,0,0,0,0].qsort!((a,b) => a < b) == [0,0,0,0,0]);
 }
