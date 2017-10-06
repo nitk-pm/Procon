2810b136f7aad86f937669dc26e0b9be1fd8a7ee
rucm <st16423@kagawa.kosen-ac.jp>
2017-10-06 13:34:38 +0900

    add test server

---
+++ tools/server/server.py
@@ -0,0 +1,32 @@
+from flask import (
+    Flask,
+    render_template,
+    send_file,
+    abort
+)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/download/<path>')
+def DownloadFile(path=None):
+    if path is None:
+        abort(400)
+    try:
+        return send_file('{}/{}'.format('data', path))
+    except Exception as e:
+        abort(400)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='8080')
