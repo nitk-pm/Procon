305269949ca7ced4057cc1064c406b09baa4f739
Unknown <namachan10777@gmail.com>
2017-10-08 20:00:21 +0900

    fix test script

--- solver/namachan/test.sh
+++ solver/namachan/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source ~/dlang/dmd-2.076.0/activate
-dub test --root=`dirname $0` --compiler=dmd
+dub test --root=`dirname $0`/solver --compiler=dmd
+dub test --root=`dirname $0`/mergetool --compiler=dmd
