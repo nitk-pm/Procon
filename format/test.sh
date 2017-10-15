e085606bd5e18670755dcc67ca15ce89460b2528
Unknown <namachan10777@gmail.com>
2017-10-15 16:43:53 +0900

    remove unsed schema files
    統合前のファイルの削除とCIの対応

--- format/test.sh
+++ format/test.sh
@@ -1,5 +1,4 @@
 HERE=$(dirname $0)
 
-python3 $HERE/validate.py $HERE/frame-schema.yml $HERE/frame-sample.json
-python3 $HERE/validate.py $HERE/piece-schema.yml $HERE/piece-sample.json
+python3 $HERE/validate.py $HERE/geometry-schema.yml $HERE/geometry-sample.json
 python3 $HERE/validate.py $HERE/output-schema.yml $HERE/output-sample.json
