3d7cc4845ce79c276c4a73b00793cb6c2f6e0b34
Unknown <namachan10777@gmail.com>
2017-11-07 20:03:36 +0900

    add qr read function
    QRコードを読み取る昨日の追加

--- tools/integrated_ui/main.py
+++ tools/integrated_ui/main.py
@@ -11,6 +11,10 @@ from kivy.uix.popup import Popup
 from kivy.lang import Builder
 from kivy.graphics import Color
 from kivy.graphics import Rectangle
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+from PIL import Image
+import cv2
 
 class ResultCanvas(Widget):
     pass
@@ -52,34 +56,81 @@ class CodeStack(BoxLayout):
 class FilePopup(Popup):
     def __init__(self, parent,**kwargs):
         super(FilePopup, self).__init__(**kwargs)
-        print(parent)
         self.load_panel = parent
 
     def cancel(self):
         self.files = []
         self.dismiss()
 
-    def send_files(self, files):
-        self.load_panel.conv(files)
+    def send_files(self, img):
+        self.load_panel.add_shape_from_img_file(img)
         self.dismiss()
 
 class LoadPanel(TabbedPanelItem):
+
+    img_rect = None
+    code = None
+    frame = None
+
     def __init__(self, **kwargs):
         super(LoadPanel, self).__init__(**kwargs)
+        self.cap = cv2.VideoCapture(0)
+        self.update_trigger = Clock.schedule_interval(self.update, 1 / 10.)
+        self.in_qr_mode = True
+        self.camera_stop = False
 
     def open_image_select_popup(self):
         self.popup = FilePopup(self)
         self.popup.open()
         return 0
 
-    def conv(self, files):
-        self.ids.shape_image_stack.add_code([])
+    def start_adding_shape_from_img_file (self):
+        return 0
+
+    def add_shape_from_img_file(self, img):
+        self.frame = cv2.imread(img[0])
+        self.in_qr_mode = False
     
-    def add_shape_from_qr(self):
+    def add_shape_from_qr(self, code):
         return 0
     
-    def add_place_from_qr(self):
+    def add_place_from_qr(self, code):
         return 0
+
+    def reflect_img (self, img):
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        img = cv2.flip(img, 0)
+        size = self.ids.load_canvas.size
+        pos  = self.ids.load_canvas.pos
+        img = cv2.resize(img, (int(size[0]), int(size[1])))
+        texture = Texture.create(size=(img.shape[1], img.shape[0]))
+        texture.blit_buffer(img.tostring())
+        if self.img_rect == None:
+            with self.ids.load_canvas.canvas:
+                self.img_rect = Rectangle(texture=texture, pos=self.ids.load_canvas.pos, size=self.ids.load_canvas.size)
+        else:
+            self.img_rect.pos = pos
+            self.img_rect.size = size
+            self.img_rect.texture = texture
+
+    def update(self, event):
+        if self.in_qr_mode:
+            #frame = self.get_frame()
+            ret, orig = self.cap.read()
+            from pyzbar.pyzbar import decode
+            from pyzbar.pyzbar import ZBarSymbol
+            frame = cv2.cvtColor(orig, cv2.COLOR_RGB2GRAY)
+            codes = decode(Image.fromarray(frame), symbols=[ZBarSymbol.QRCODE])
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+            print(codes)
+            if len(codes) > 0 or self.code == None:
+                if len(codes) > 0:
+                    self.code = codes[0]
+                self.frame = orig
+            self.reflect_img(self.frame)
+                
+
+        
 
 Builder.load_file('ui.kv')
 
@@ -88,4 +139,6 @@ class MainApp(App):
         return Base()
 
 if __name__ == '__main__':
+    from kivy.config import Config
+    Config.set('input', 'mouse', 'mouse,disable_multitouch')
     MainApp().run()
