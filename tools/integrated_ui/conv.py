2193b660ac893b288ebd28b7df468275f1ee8263
Unknown <namachan10777@gmail.com>
2017-11-11 20:05:38 +0900

    impl function of strings to shape infomation's dict
    QRコードのコードから辞書を返す関数の実装

--- tools/integrated_ui/conv.py
+++ tools/integrated_ui/conv.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 
+import geom
+
 def parse_code (str):
     splited = str.split(':')
     piece_num = int(splited[0])
@@ -26,3 +28,21 @@ def expand_piece (base_shape):
         rotateds.append(rotated)
         rotateds.append(geom.invert(rotated))
     return rotateds
+
+def compile_codes_to_dict(codes):
+    all_pieces = []
+    all_frames = []
+    def ctor_shapes(shapes):
+        def ctor_points(p):
+            return {'x': p[0][0], 'y':[1][0]}
+        return list(map(lambda shape:
+            list(map(ctor_points, shape)), shapes))
+    for code in codes:
+        pieces, frames = parse_code(code)
+        for piece in pieces:
+            all_pieces.append(ctor_shapes(expand_piece(piece)))
+        for frame in frames:
+            all_frames.append(frame)
+    piece_dict = {'pieces': all_pieces}
+    frame_dict = {'frames': ctor_shapes(all_frames)}
+    return piece_dict, frame_dict
