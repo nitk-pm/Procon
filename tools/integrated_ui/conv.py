1466859f660d97e6d2bce6833c31195d14f609cc
Nakano Masaki <namachan10777@gmail.com>
2017-11-11 18:30:58 +0900

    add txt code parser

---
+++ tools/integrated_ui/conv.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def parse_code (str):
+    splited = str.split(':')
+    piece_num = int(splited[0])
+    shapes    = list(map(
+        lambda s: list(map(lambda num: float(num),
+            filter (lambda num: num != '', s.split(' ')))),
+        splited[1:]))
+    def shape_ctor(nums):
+        assert len(nums) % 2 == 0, "illegal data"
+        shape = []
+        for i in range(0, len(nums), 2):
+            shape.append(np.asarray([[nums[i]], [nums[i+1]]]))
+        return shape
+    pieces_base = shapes[:piece_num]
+    frames_base = shapes[piece_num:]
+    return {'pieces': list(map(shape_ctor, pieces_base)), 'frames': list(map(shape_ctor, frames_base))}
